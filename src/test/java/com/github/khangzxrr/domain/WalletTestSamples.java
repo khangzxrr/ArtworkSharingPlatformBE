ed26d7b3a5618fa46c22607bbfeb5ab32e977abd
khangzxrr <khangzxrr@gmail.com>
2024-02-09 14:44:04 +0700

    comment sample IT

--- src/test/java/com/github/khangzxrr/domain/WalletTestSamples.java
+++ src/test/java/com/github/khangzxrr/domain/WalletTestSamples.java
@@ -1,22 +1,22 @@
-package com.github.khangzxrr.domain;
+// package com.github.khangzxrr.domain;
 
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
+// import java.util.Random;
+// import java.util.concurrent.atomic.AtomicLong;
 
-public class WalletTestSamples {
+// public class WalletTestSamples {
 
-    private static final Random random = new Random();
-    private static final AtomicLong longCount = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
+//     private static final Random random = new Random();
+//     private static final AtomicLong longCount = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
 
-    public static Wallet getWalletSample1() {
-        return new Wallet().id(1L).amount(1L);
-    }
+//     public static Wallet getWalletSample1() {
+//         return new Wallet().id(1L).amount(1L);
+//     }
 
-    public static Wallet getWalletSample2() {
-        return new Wallet().id(2L).amount(2L);
-    }
+//     public static Wallet getWalletSample2() {
+//         return new Wallet().id(2L).amount(2L);
+//     }
 
-    public static Wallet getWalletRandomSampleGenerator() {
-        return new Wallet().id(longCount.incrementAndGet()).amount(longCount.incrementAndGet());
-    }
-}
+//     public static Wallet getWalletRandomSampleGenerator() {
+//         return new Wallet().id(longCount.incrementAndGet()).amount(longCount.incrementAndGet());
+//     }
+// }
