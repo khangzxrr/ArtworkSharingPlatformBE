516f5b32fff4296209ddb6245e9d747b7eb7e313
khangzxrr <khangzxrr@gmail.com>
2024-02-09 14:40:12 +0700

    add spacer

--- src/main/java/com/github/khangzxrr/service/UserService.java
+++ src/main/java/com/github/khangzxrr/service/UserService.java
@@ -114,6 +114,7 @@ public class UserService {
         newUser.setActivated(false);
         // new user gets registration key
         newUser.setActivationKey(RandomUtil.generateActivationKey());
+
         Set<Authority> authorities = new HashSet<>();
         authorityRepository.findById(AuthoritiesConstants.USER).ifPresent(authorities::add);
         newUser.setAuthorities(authorities);
