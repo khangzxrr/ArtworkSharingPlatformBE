88e5539a59dea43daeef987df890a1541884419d
khangzxrr <khangzxrr@gmail.com>
2024-02-23 17:03:06 +0700

    replace getAll instead of getAllOfUser

--- src/main/java/com/github/khangzxrr/web/rest/RequestResourceOfAudience.java
+++ src/main/java/com/github/khangzxrr/web/rest/RequestResourceOfAudience.java
@@ -102,7 +102,7 @@ public class RequestResourceOfAudience {
     public ResponseEntity<List<RequestDTO>> getAllRequests(@org.springdoc.core.annotations.ParameterObject Pageable pageable) {
         log.debug("REST request to get a page of Requests belong to audience");
 
-        Page<RequestDTO> page = requestService.getAllOfUser(pageable);
+        Page<RequestDTO> page = requestService.getAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
         return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
@@ -118,7 +118,7 @@ public class RequestResourceOfAudience {
     @Transactional(readOnly = true)
     public ResponseEntity<RequestDTO> getRequest(@PathVariable("id") Long id) {
         log.debug("REST request to get Request : {}", id);
-        Optional<Request> request = requestService.getOneOfUser(id);
+        Optional<Request> request = requestService.getOne(id);
 
         return ResponseUtil.wrapOrNotFound(request.map(requestMapper::toDto));
     }
