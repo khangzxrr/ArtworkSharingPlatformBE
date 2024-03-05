636a816a8ac7bf4406465c415c7e20c5d9f821d6
CaoHieu151515 <caotronghieu151515@gmail.com>
2024-03-06 01:46:00 +0700

    CURD of Post

--- src/main/java/com/github/khangzxrr/service/impl/ArtworkCommentServiceImpl.java
+++ src/main/java/com/github/khangzxrr/service/impl/ArtworkCommentServiceImpl.java
@@ -1,10 +1,15 @@
 package com.github.khangzxrr.service.impl;
 
 import com.github.khangzxrr.domain.ArtworkComment;
+import com.github.khangzxrr.domain.User;
 import com.github.khangzxrr.repository.ArtworkCommentRepository;
 import com.github.khangzxrr.service.ArtworkCommentService;
+import com.github.khangzxrr.service.UserService;
 import com.github.khangzxrr.service.dto.ArtworkCommentDTO;
 import com.github.khangzxrr.service.mapper.ArtworkCommentMapper;
+import com.github.khangzxrr.web.rest.errors.BadRequestIDAlertException;
+import com.github.khangzxrr.web.rest.errors.NotLoggedException;
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -27,15 +32,30 @@ public class ArtworkCommentServiceImpl implements ArtworkCommentService {
 
     private final ArtworkCommentMapper artworkCommentMapper;
 
-    public ArtworkCommentServiceImpl(ArtworkCommentRepository artworkCommentRepository, ArtworkCommentMapper artworkCommentMapper) {
+    private final UserService userService;
+
+    public ArtworkCommentServiceImpl(
+        ArtworkCommentRepository artworkCommentRepository,
+        ArtworkCommentMapper artworkCommentMapper,
+        UserService userService
+    ) {
         this.artworkCommentRepository = artworkCommentRepository;
         this.artworkCommentMapper = artworkCommentMapper;
+        this.userService = userService;
     }
 
     @Override
     public ArtworkCommentDTO save(ArtworkCommentDTO artworkCommentDTO) {
         log.debug("Request to save ArtworkComment : {}", artworkCommentDTO);
         ArtworkComment artworkComment = artworkCommentMapper.toEntity(artworkCommentDTO);
+
+        Optional<User> userOptional = userService.getUserWithAuthorities();
+        if (!userOptional.isPresent()) {
+            throw new NotLoggedException();
+        }
+        artworkComment.setOwner(userOptional.get());
+        artworkComment.setCreateAt(LocalDate.now());
+
         artworkComment = artworkCommentRepository.save(artworkComment);
         return artworkCommentMapper.toDto(artworkComment);
     }
@@ -44,6 +64,18 @@ public class ArtworkCommentServiceImpl implements ArtworkCommentService {
     public ArtworkCommentDTO update(ArtworkCommentDTO artworkCommentDTO) {
         log.debug("Request to update ArtworkComment : {}", artworkCommentDTO);
         ArtworkComment artworkComment = artworkCommentMapper.toEntity(artworkCommentDTO);
+
+        Long cmtid = artworkComment.getId();
+
+        Optional<User> userOptional = userService.getUserWithAuthorities();
+        if (!userOptional.isPresent()) {
+            throw new NotLoggedException();
+        }
+
+        artworkComment.setOwner(userOptional.get());
+
+        checkOwner(cmtid, userOptional.get());
+
         artworkComment = artworkCommentRepository.save(artworkComment);
         return artworkCommentMapper.toDto(artworkComment);
     }
@@ -84,6 +116,24 @@ public class ArtworkCommentServiceImpl implements ArtworkCommentService {
     @Override
     public void delete(Long id) {
         log.debug("Request to delete ArtworkComment : {}", id);
+
+        Optional<User> userOptional = userService.getUserWithAuthorities();
+        if (!userOptional.isPresent()) {
+            throw new NotLoggedException();
+        }
+
+        checkOwner(id, userOptional.get());
+
         artworkCommentRepository.deleteById(id);
+    }
+
+    private void checkOwner(Long cmtid, User currentUser) {
+        ArtworkComment artworkComment = artworkCommentRepository
+            .findById(cmtid)
+            .orElseThrow(() -> new BadRequestIDAlertException("Comment not found with ID: ", cmtid, "Comment_not_found"));
+
+        if (!artworkComment.getOwner().getId().equals(currentUser.getId())) {
+            throw new BadRequestIDAlertException("Current user is not the owner of ArtworkComment with ID: ", cmtid, "Owner_not_match");
+        }
     }
 }
--- src/main/java/com/github/khangzxrr/web/rest/ArtworkPostResourse.java
+++ src/main/java/com/github/khangzxrr/web/rest/ArtworkPostResourse.java
@@ -1,7 +1,10 @@
 package com.github.khangzxrr.web.rest;
 
+import com.github.khangzxrr.repository.ArtworkCommentRepository;
 import com.github.khangzxrr.repository.ArtworkRepository;
+import com.github.khangzxrr.service.ArtworkCommentService;
 import com.github.khangzxrr.service.ArtworkService;
+import com.github.khangzxrr.service.dto.ArtworkCommentDTO;
 import com.github.khangzxrr.service.dto.ArtworkDTO;
 import com.github.khangzxrr.web.rest.errors.BadRequestAlertException;
 import java.net.URI;
@@ -31,7 +34,7 @@ public class ArtworkPostResourse {
 
     private final Logger log = LoggerFactory.getLogger(ArtworkResource.class);
 
-    private static final String ENTITY_NAME = "artwork";
+    private static final String ENTITY_NAME = "artworkComment";
 
     @Value("${jhipster.clientApp.name}")
     private String applicationName;
@@ -40,9 +43,20 @@ public class ArtworkPostResourse {
 
     private final ArtworkRepository artworkRepository;
 
-    public ArtworkPostResourse(ArtworkService artworkService, ArtworkRepository artworkRepository) {
+    private final ArtworkCommentService artworkCommentService;
+
+    private final ArtworkCommentRepository artworkCommentRepository;
+
+    public ArtworkPostResourse(
+        ArtworkService artworkService,
+        ArtworkRepository artworkRepository,
+        ArtworkCommentService artworkCommentService,
+        ArtworkCommentRepository artworkCommentRepository
+    ) {
         this.artworkService = artworkService;
         this.artworkRepository = artworkRepository;
+        this.artworkCommentService = artworkCommentService;
+        this.artworkCommentRepository = artworkCommentRepository;
     }
 
     @GetMapping("/getAll/likeAndcmt/{id}")
@@ -50,5 +64,60 @@ public class ArtworkPostResourse {
         log.debug("REST request to get Artwork : {}", id);
         Optional<ArtworkDTO> artworkDTO = artworkService.findAllPostLike(id);
         return ResponseUtil.wrapOrNotFound(artworkDTO);
+    }
+
+    @PostMapping("/user/commentArtwork")
+    public ResponseEntity<ArtworkCommentDTO> createArtworkComment(@RequestBody ArtworkCommentDTO artworkCommentDTO)
+        throws URISyntaxException {
+        log.debug("REST request to save ArtworkComment : {}", artworkCommentDTO);
+        if (artworkCommentDTO.getId() != null) {
+            throw new BadRequestAlertException("A new artworkComment cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        ArtworkCommentDTO result = artworkCommentService.save(artworkCommentDTO);
+        return ResponseEntity
+            .created(new URI("/api/artwork-comments/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArtworkCommentDTO> getArtworkComment(@PathVariable("id") Long id) {
+        log.debug("REST request to get ArtworkComment : {}", id);
+        Optional<ArtworkCommentDTO> artworkCommentDTO = artworkCommentService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(artworkCommentDTO);
+    }
+
+    @PutMapping("/user/editThierPost/{id}")
+    public ResponseEntity<ArtworkCommentDTO> updateArtworkComment(
+        @PathVariable(value = "id", required = false) final Long id,
+        @RequestBody ArtworkCommentDTO artworkCommentDTO
+    ) throws URISyntaxException {
+        log.debug("REST request to update ArtworkComment : {}, {}", id, artworkCommentDTO);
+        if (artworkCommentDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, artworkCommentDTO.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!artworkCommentRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+
+        ArtworkCommentDTO result = artworkCommentService.update(artworkCommentDTO);
+        return ResponseEntity
+            .ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, artworkCommentDTO.getId().toString()))
+            .body(result);
+    }
+
+    @DeleteMapping("/user/delete/cmt/{id}")
+    public ResponseEntity<Void> deleteArtworkComment(@PathVariable("id") Long id) {
+        log.debug("REST request to delete ArtworkComment : {}", id);
+        artworkCommentService.delete(id);
+        return ResponseEntity
+            .noContent()
+            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, id.toString()))
+            .build();
     }
 }
