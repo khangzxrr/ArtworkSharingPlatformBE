9a287ef8760f99f2db476d48d0f92feae36ddcb6
khangzxrr <khangzxrr@gmail.com>
2024-03-05 20:53:26 +0700

    add notification API

--- src/main/java/com/github/khangzxrr/service/NotificationService.java
+++ src/main/java/com/github/khangzxrr/service/NotificationService.java
@@ -4,5 +4,9 @@ import com.github.khangzxrr.domain.User;
 import java.util.Map;
 
 public interface NotificationService {
+    public void subcribeUsersToTopic(String topic, User... users);
+
     public void sendToUser(Map<String, String> data, User user);
+
+    public void sendToUsers(String title, String body, User... users);
 }
--- src/main/java/com/github/khangzxrr/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/github/khangzxrr/service/impl/NotificationServiceImpl.java
@@ -6,19 +6,37 @@ import com.github.khangzxrr.service.NotificationService;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class NotificationServiceImpl implements NotificationService {
 
+    private final Logger log = LoggerFactory.getLogger(NotificationServiceImpl.class);
+
     private final FirebaseMessaging firebaseMessaging;
 
     public NotificationServiceImpl(FirebaseMessaging firebaseMessaging) {
         this.firebaseMessaging = firebaseMessaging;
+    }
+
+    public List<String> mapUsersToTokens(@NotNull User[] users) {
+        List<UserNotifyToken> notifitokens = Arrays.stream(users).map(u -> u.getNotifyTokens()).flatMap(Set::stream).toList();
+
+        List<String> tokens = notifitokens.stream().map(n -> n.getToken()).toList();
+
+        return tokens;
     }
 
     @Override
@@ -31,6 +49,34 @@ public class NotificationServiceImpl implements NotificationService {
 
                 firebaseMessaging.send(message);
             } catch (FirebaseMessagingException firebaseMessagingException) {}
+        }
+    }
+
+    @Override
+    public void sendToUsers(@NotBlank String title, @NotBlank String body, @NotNull User... users) {
+        List<String> tokens = mapUsersToTokens(users);
+
+        Notification notification = Notification.builder().setTitle(title).setBody(body).build();
+
+        MulticastMessage message = MulticastMessage.builder().setNotification(notification).addAllTokens(tokens).build();
+
+        try {
+            firebaseMessaging.sendEachForMulticast(message);
+        } catch (FirebaseMessagingException ex) {
+            log.error(ex.getMessage(), ex);
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void subcribeUsersToTopic(@NotBlank String topic, @NotNull User... users) {
+        List<String> tokens = mapUsersToTokens(users);
+
+        try {
+            firebaseMessaging.subscribeToTopic(tokens, topic);
+        } catch (FirebaseMessagingException e) {
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
         }
     }
 }
