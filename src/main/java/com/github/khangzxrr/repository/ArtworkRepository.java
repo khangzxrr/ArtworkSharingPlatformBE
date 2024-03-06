f83406c696b35350561432dc52fdfb26cc8410cc
khangzxrr <khangzxrr@gmail.com>
2024-03-06 13:32:28 +0700

    add filter by visibility

--- src/main/java/com/github/khangzxrr/repository/ArtworkRepository.java
+++ src/main/java/com/github/khangzxrr/repository/ArtworkRepository.java
@@ -1,7 +1,10 @@
 package com.github.khangzxrr.repository;
 
 import com.github.khangzxrr.domain.Artwork;
+import com.github.khangzxrr.domain.enumeration.ArtworkVisibility;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +15,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArtworkRepository extends JpaRepository<Artwork, Long> {
     @Query("select artwork from Artwork artwork where artwork.owner.login = ?#{authentication.name}")
-    List<Artwork> findByOwnerIsCurrentUser();
+    Page<Artwork> findByOwnerIsCurrentUser(Pageable pageable);
+
+    Page<Artwork> findByVisibility(ArtworkVisibility artworkVisibility, Pageable pageable);
 }
