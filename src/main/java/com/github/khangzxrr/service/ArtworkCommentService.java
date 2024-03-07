38e88d4596a6b3b44313555753b9eaa1aaa1c13b
khangzxrr <khangzxrr@gmail.com>
2024-03-07 08:17:51 +0700

    remove unuse method

--- src/main/java/com/github/khangzxrr/service/ArtworkCommentService.java
+++ src/main/java/com/github/khangzxrr/service/ArtworkCommentService.java
@@ -1,56 +1,16 @@
 package com.github.khangzxrr.service;
 
 import com.github.khangzxrr.service.dto.ArtworkCommentDTO;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Service Interface for managing {@link com.github.khangzxrr.domain.ArtworkComment}.
  */
 public interface ArtworkCommentService {
-    /**
-     * Save a artworkComment.
-     *
-     * @param artworkCommentDTO the entity to save.
-     * @return the persisted entity.
-     */
-    ArtworkCommentDTO save(ArtworkCommentDTO artworkCommentDTO);
+    Page<ArtworkCommentDTO> getAll(Long artworkId, Pageable pageable);
 
-    /**
-     * Updates a artworkComment.
-     *
-     * @param artworkCommentDTO the entity to update.
-     * @return the persisted entity.
-     */
-    ArtworkCommentDTO update(ArtworkCommentDTO artworkCommentDTO);
+    ArtworkCommentDTO comment(Long id, CreateArtworkCommentDTO createArtworkCommentDTO);
 
-    /**
-     * Partially updates a artworkComment.
-     *
-     * @param artworkCommentDTO the entity to update partially.
-     * @return the persisted entity.
-     */
-    Optional<ArtworkCommentDTO> partialUpdate(ArtworkCommentDTO artworkCommentDTO);
-
-    /**
-     * Get all the artworkComments.
-     *
-     * @return the list of entities.
-     */
-    List<ArtworkCommentDTO> findAll();
-
-    /**
-     * Get the "id" artworkComment.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    Optional<ArtworkCommentDTO> findOne(Long id);
-
-    /**
-     * Delete the "id" artworkComment.
-     *
-     * @param id the id of the entity.
-     */
-    void delete(Long id);
+    void delete(Long artworkId, Long commentId);
 }
