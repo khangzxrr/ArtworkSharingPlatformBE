0d05784ed3b6ec05ac2f7f8b370b341ed808a4ab
khangzxrr <khangzxrr@gmail.com>
2024-03-09 22:59:28 +0700

    add getLike method

--- src/main/java/com/github/khangzxrr/service/ArtworkLikeService.java
+++ src/main/java/com/github/khangzxrr/service/ArtworkLikeService.java
@@ -1,9 +1,13 @@
 package com.github.khangzxrr.service;
 
+import com.github.khangzxrr.service.dto.ArtworkLikeDTO;
+import java.util.Optional;
+
 /**
  * Service Interface for managing {@link com.github.khangzxrr.domain.ArtworkLike}.
  */
 public interface ArtworkLikeService {
     void Like(Long id);
     void Unlike(Long id);
+    Optional<ArtworkLikeDTO> getLikeByUser(Long artworkId);
 }
