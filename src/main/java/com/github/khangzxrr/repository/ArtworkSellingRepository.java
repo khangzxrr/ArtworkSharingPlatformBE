4c09d0153228a4d51ec277dc24307a013f1e73d1
khangzxrr <khangzxrr@gmail.com>
2024-03-10 13:24:20 +0700

    find all by status in list, and LOCK with READ

--- src/main/java/com/github/khangzxrr/repository/ArtworkSellingRepository.java
+++ src/main/java/com/github/khangzxrr/repository/ArtworkSellingRepository.java
@@ -1,6 +1,12 @@
 package com.github.khangzxrr.repository;
 
 import com.github.khangzxrr.domain.ArtworkSelling;
+import com.github.khangzxrr.domain.enumeration.ArtworkSellingStatus;
+import com.github.khangzxrr.domain.enumeration.ArtworkSellingType;
+import jakarta.persistence.LockModeType;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +15,7 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface ArtworkSellingRepository extends JpaRepository<ArtworkSelling, Long> {}
+public interface ArtworkSellingRepository extends JpaRepository<ArtworkSelling, Long> {
+    @Lock(LockModeType.PESSIMISTIC_READ) //this is important, because we must LOCK entity to perform any changes (dont want any other transaction update, reading this record)
+    Set<ArtworkSelling> findAllByStatusIn(List<ArtworkSellingStatus> status);
+}
