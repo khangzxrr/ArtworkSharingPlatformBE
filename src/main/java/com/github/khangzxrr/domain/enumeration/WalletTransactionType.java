acc96c60117856c49b3ef762c7a4e6c8e00a732e
khangzxrr <khangzxrr@gmail.com>
2024-03-13 16:24:53 +0700

    add new transaction type

--- src/main/java/com/github/khangzxrr/domain/enumeration/WalletTransactionType.java
+++ src/main/java/com/github/khangzxrr/domain/enumeration/WalletTransactionType.java
@@ -6,10 +6,16 @@ package com.github.khangzxrr.domain.enumeration;
 public enum WalletTransactionType {
     DEPOSIT,
     WITHDRAWAL,
+
     BUY,
+    DIRECT_BUY_ARTWORK,
+
     REFUND,
     REQUEST_EARN,
+    ARTWORK_SELL_EARN,
+
     SERVICE_FEE_EARN,
+
     REQUEST_FIRST_PAYMENT_TEMP,
     WITHDRAW_REQUEST_FIRST_PAYMENT_TEMP,
     WITHDRAW_REFUND_REQUEST_FIRST_PAYMENT_TEMP,
