61b3b3a96f2237853b394ac27b32856af07d64d5
khangzxrr <khangzxrr@gmail.com>
2024-03-13 16:25:46 +0700

    locking wallet

--- src/main/java/com/github/khangzxrr/repository/WalletRepository.java
+++ src/main/java/com/github/khangzxrr/repository/WalletRepository.java
@@ -1,6 +1,7 @@
 package com.github.khangzxrr.repository;
 
 import com.github.khangzxrr.domain.Wallet;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
@@ -10,11 +11,14 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select wallet from Wallet wallet where wallet.user.login = 'admin'")
     Optional<Wallet> findByAdmin();
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select wallet from Wallet wallet where wallet.user.login = ?#{authentication.name}")
     Optional<Wallet> findByUserIsCurrentUser();
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Wallet> findByUserLogin(String login);
 }
